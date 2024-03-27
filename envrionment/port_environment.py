8efaacdaa935aaeda860d60419aa1bbe44902251
LeeXzhou <109325527+LeeXzhou@users.noreply.github.com>
2024-03-27 17:19:48 +0800

    update read map function & mapsps

--- envrionment/port_environment.py
+++ envrionment/port_environment.py
@@ -1,5 +1,7 @@
 import gym
 import torch
+import numpy as np
+import utils
 
 
 class Port(gym.Env):
@@ -8,15 +10,38 @@ class Port(gym.Env):
         'video.frames_per_second': 2
     }
 
-    def __init__(self):
+    def __init__(self, map_path: str, seed: int = 0) -> None:
+        '''
+        :param map_path: read map file path
+        :param seed: for random generating goods
+        map description:
+        . -> 0 land
+        * -> -1 sea
+        # -> -2 obstacle
+        A -> -3 robot position
+        B -> -4 port position, consist of 4 * 4 points
+        a positive number means that here is a good and the number means its value
+        '''
         self.action_space = None
         self.observation_space = None
+        self.map_path = map_path
+        self.map = np.zeros((200, 200))
+        self.score = 0
+        self.reset()
         pass
 
-    def step(self, action):
-        return self.state, reward, done, {}
+    def step(self, actions):
+        for action in actions:
+            actor = action[:4]
+            if actor == "robo":
+                None
+            elif actor == "ship":
+                None
+        return self.state
 
     def reset(self):
+        self.map = utils.map_translator(self.map_path)
+        self.score = 0
         return self.state
 
     def render(self, mode='human'):
---
+++ envrionment/utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def map_translator(map_path : str) -> np.array:
+    '''
+    :param map_path: map file path
+    :return: map
+    '''
+    map_dict = {'.' : 0, '*' : -1, '#' : -2, 'A' : -3, 'B' : -4}
+    ret = np.zeros((200, 200))
+    with open(map_path, 'r') as file:
+        lines = file.readlines()
+        row = 0
+        column = 0
+        for line in lines:
+            for char in line:
+                ret[row][column] = map_dict[char]
+                column += 1
+            row += 1
+            column = 0
+    return ret
+def show_map(map : np.array) -> None:
+    '''
+    :param map: map information
+    '''
+    img=np.zeros((600,600),dtype=np.uint8)
+    for i in range(0, 200):
+        for j in range(0, 200):
+            tp = (0, 0, 0)
+            if map[i][j] == 0:
+                tp = (100, 240, 100)
+            elif map[i][j] == -1:
+                tp = (150, 200, 255)
+            elif map[i][j] == -2:
+                tp = (128, 128, 128)
+            elif map[i][j] == -3:
+                tp = (255, 255, 255)
+            else:
+                tp = (0, 0, 0)
+            for u in range(0, 3):
+                for v in range(0, 3):
+                    img[i * 3 + u][j * 3 + v] = tp
+    plt.imshow(img)
+    plt.show()
+
+
