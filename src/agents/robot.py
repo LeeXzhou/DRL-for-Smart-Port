d2170ee17f00fc8ecf93ff3db7e331beff49c7a4
From Group Server <yebt2022@shanghaitech.edu.cn>
2024-04-14 23:55:36 +0800

    feat: init classes

--- src/agents/robot.py
+++ src/agents/robot.py
@@ -4,8 +4,12 @@ import numpy as np
 class Robot:
     """robot move can be controlled by rl but get and pull action cannot???"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, id: int, init_x: int, init_y: int) -> None:
+        self.id = id
+        self.goods = 0
+        self.pos = [init_x, init_y]
+        # self.wait_time = 0
 
-    def get_move_action():
-        return 0
+    def act(self):
+        """output all actions of robot including move, get, and pull"""
+        pass
--- src/entities/berth.py
+++ src/entities/berth.py
@@ -1,4 +1,10 @@
+from collections import deque
+
+
 class Berth:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, loading_speed: int, transport_time: int) -> None:
+        self.free = True
+        self.loading_speed = loading_speed
+        self.transport_time = transport_time
+        self.goods = deque()
